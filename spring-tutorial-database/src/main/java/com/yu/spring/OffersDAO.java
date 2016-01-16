49b822c405b4e1966d92ef8dc91761c8b73d6f3f
xiaoyuxqx <xiaoyuxqx@gmail.com>
2016-01-16 02:05:06 -0600

    Replace tabs with spaces

--- spring-tutorial-database/src/main/java/com/yu/spring/OffersDAO.java
+++ spring-tutorial-database/src/main/java/com/yu/spring/OffersDAO.java
@@ -19,112 +19,117 @@ import org.springframework.transaction.annotation.Transactional;
 @Component("offersDao")
 public class OffersDAO {
 
-	private NamedParameterJdbcTemplate jdbc;
+    private NamedParameterJdbcTemplate jdbc;
 
-	/**
-	 * JDBC template wrap the data source. data source is coming from bean
-	 * dataSource
-	 * 
-	 * @param jdbc
-	 */
-	@Autowired
-	public void setDataSource(DataSource jdbc) {
-		this.jdbc = new NamedParameterJdbcTemplate(jdbc);
-	}
+    /**
+     * JDBC template wrap the data source. data source is coming from bean
+     * dataSource
+     * 
+     * @param jdbc
+     */
+    @Autowired
+    public void setDataSource(DataSource jdbc) {
+        this.jdbc = new NamedParameterJdbcTemplate(jdbc);
+    }
 
-	/**
-	 * This method create a anonymous {@link RowMapper} specify the routine of
-	 * generating Offer Object given retrieved database query result.
-	 * 
-	 * @return
-	 */
-	public List<Offer> getOffers() {
-		// Prefix placehold by column and name.
-		return jdbc.query("select * from offers", new RowMapper<Offer>() {
+    /**
+     * This method create a anonymous {@link RowMapper} specify the routine of
+     * generating Offer Object given retrieved database query result.
+     * 
+     * @return
+     */
+    public List<Offer> getOffers() {
+        // Prefix placehold by column and name.
+        return jdbc.query("select * from offers", new RowMapper<Offer>() {
 
-			public Offer mapRow(ResultSet rs, int rowNum) throws SQLException {
-				Offer offer = new Offer();
-				offer.setId(rs.getInt("id"));
-				offer.setName(rs.getString("name"));
-				offer.setEmail(rs.getString("email"));
-				offer.setText(rs.getString("text"));
-				return offer;
-			}
+            public Offer mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Offer offer = new Offer();
+                offer.setId(rs.getInt("id"));
+                offer.setName(rs.getString("name"));
+                offer.setEmail(rs.getString("email"));
+                offer.setText(rs.getString("text"));
+                return offer;
+            }
 
-		});
-	}
+        });
+    }
 
-	/**
-	 * Create an {@link Offer} entry to database.
-	 * @param offer
-	 * @return
-	 */
-	public boolean create(Offer offer) {
-		// It will look into the bean for properties.
-		BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(
-		        offer);
+    /**
+     * Create an {@link Offer} entry to database.
+     * 
+     * @param offer
+     * @return
+     */
+    public boolean create(Offer offer) {
+        // It will look into the bean for properties.
+        BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(
+                offer);
 
-		return jdbc.update("insert into offers (name, text, email) values (:name, :text, :email)", params) == 1;
-	}
-	
-	/**
-	 * Create multiple {@link Offer} entries to database.
-	 * Transaction annotation makes sure only all database operations success, then the changes commit, otherwise
-	 * roll back.
-	 * @param offers
-	 * @return
-	 */
-	@Transactional
-	public int[] create(List<Offer> offers) {
-		SqlParameterSource[] params = SqlParameterSourceUtils.createBatch(offers.toArray());
-		
-		return jdbc.batchUpdate("insert into offers (name, text, email) values (:name, :text, :email)", params);
-	}
+        return jdbc.update("insert into offers (name, text, email) values (:name, :text, :email)", params) == 1;
+    }
 
-	/**
-	 * Delete a given id from database.
-	 * 
-	 * @param id
-	 * @return Number of row affected
-	 */
-	public int delete(int id) {
-		MapSqlParameterSource params = new MapSqlParameterSource("id", id);
+    /**
+     * Create multiple {@link Offer} entries to database. Transaction annotation
+     * makes sure only all database operations success, then the changes commit,
+     * otherwise roll back.
+     * 
+     * @param offers
+     * @return
+     */
+    @Transactional
+    public int[] create(List<Offer> offers) {
+        SqlParameterSource[] params = SqlParameterSourceUtils.createBatch(offers.toArray());
 
-		return jdbc.update("delete from offers where id=:id", params);
-	}
+        return jdbc.batchUpdate("insert into offers (name, text, email) values (:name, :text, :email)", params);
+    }
 
-	/**
-	 * Update an entry {@link Offer} to database.
-	 * @param offer
-	 * @return
-	 */
-	public boolean update(Offer offer) {
-		// It will look into the bean for properties.
-		BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(
-		        offer);
+    /**
+     * Delete a given id from database.
+     * 
+     * @param id
+     * @return Number of row affected
+     */
+    public int delete(int id) {
+        MapSqlParameterSource params = new MapSqlParameterSource("id", id);
 
-		return jdbc.update("update offers set name=:name, text=:text, email=:email where id=:id", params) == 1;
+        return jdbc.update("delete from offers where id=:id", params);
+    }
 
-	}
+    /**
+     * Update an entry {@link Offer} to database.
+     * 
+     * @param offer
+     * @return
+     */
+    public boolean update(Offer offer) {
+        // It will look into the bean for properties.
+        BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(
+                offer);
 
-	public Offer getOffer(int id) {
-		// "id" <--> :id, Sue is the replace string.
-		// Use params.addValue to add many placeholders.
-		MapSqlParameterSource params = new MapSqlParameterSource();
-		params.addValue("id", id);
-		// Prefix placeholder by column and name.
-		return jdbc.queryForObject("select * from offers where id = :id",
-		        params, new RowMapper<Offer>() {
-			        public Offer mapRow(ResultSet rs, int rowNum)
-			                throws SQLException {
-				        Offer offer = new Offer();
-				        offer.setId(rs.getInt("id"));
-				        offer.setName(rs.getString("name"));
-				        offer.setEmail(rs.getString("email"));
-				        offer.setText(rs.getString("text"));
-				        return offer;
-			        }
+        return jdbc
+                .update("update offers set name=:name, text=:text, email=:email where id=:id",
+                        params) == 1;
 
-		        });
-	}
+    }
+
+    public Offer getOffer(int id) {
+        // "id" <--> :id, Sue is the replace string.
+        // Use params.addValue to add many placeholders.
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("id", id);
+        // Prefix placeholder by column and name.
+        return jdbc.queryForObject("select * from offers where id = :id",
+                params, new RowMapper<Offer>() {
+                    public Offer mapRow(ResultSet rs, int rowNum)
+                            throws SQLException {
+                        Offer offer = new Offer();
+                        offer.setId(rs.getInt("id"));
+                        offer.setName(rs.getString("name"));
+                        offer.setEmail(rs.getString("email"));
+                        offer.setText(rs.getString("text"));
+                        return offer;
+                    }
+
+                });
+    }
 }
